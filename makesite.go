c65d37a000643c7eb143beb97dfaddd65822bf49
Vithusha Ravirajan <vithusharavirajan@Vithus-MBP.local>
2024-04-08 18:17:03 -0400

    v1 partially complete

--- makesite.go
+++ makesite.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	// "fmt"
+	"log"
+	"os"
+	"strings"
+	"text/template"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	fileName := flag.String("file", "first-post.txt", "The name of the text file to read in")
+	flag.Parse()
+
+	file, err := os.Open(*fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan() // This reads the first line, which is the title
+	title := scanner.Text()
+
+	content := ""
+	for scanner.Scan() {
+		content += scanner.Text() + "\n"
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	tmpl, err := template.ParseFiles("template.tmpl")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data := struct {
+		Title   string
+		Content string
+	}{
+		Title:   title,
+		Content: content,
+	}
+
+	outputFileName := strings.TrimSuffix(*fileName, ".txt") + ".html"
+	outFile, err := os.Create(outputFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer outFile.Close()
+
+	err = tmpl.Execute(outFile, data)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
+
