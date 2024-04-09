630d58f161f6dd9bdc4def17e92ff09121e1684e
Vithusha Ravirajan <vithusharavirajan@Vithus-MBP.local>
2024-04-08 23:39:53 -0400

    v1.1 complete

--- makesite.go
+++ makesite.go
@@ -3,18 +3,34 @@ package main
 import (
 	"bufio"
 	"flag"
-	// "fmt"
+	"fmt"
+	// "io/fs"
+	"io/ioutil"
 	"log"
 	"os"
 	"strings"
 	"text/template"
+	"time"
 )
 
-func main() {
-	fileName := flag.String("file", "first-post.txt", "The name of the text file to read in")
-	flag.Parse()
+func findTxtFiles(dirPath string) []string {
+	var files []string
+	entries, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	file, err := os.Open(*fileName)
+	for _, entry := range entries {
+		if !entry.IsDir() && strings.HasSuffix(entry.Name(), ".txt") {
+			files = append(files, entry.Name())
+		}
+	}
+
+	return files
+}
+
+func generateHTML(fileName string, tmpl *template.Template) {
+	file, err := os.Open(fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -33,11 +49,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	tmpl, err := template.ParseFiles("template.tmpl")
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	data := struct {
 		Title   string
 		Content string
@@ -46,7 +57,7 @@ func main() {
 		Content: content,
 	}
 
-	outputFileName := strings.TrimSuffix(*fileName, ".txt") + ".html"
+	outputFileName := strings.TrimSuffix(fileName, ".txt") + ".html"
 	outFile, err := os.Create(outputFileName)
 	if err != nil {
 		log.Fatal(err)
@@ -57,5 +68,32 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func main() {
+	dirPtr := flag.String("dir", ".", "Directory to search for .txt files")
+	flag.Parse()
+
+	tmpl, err := template.ParseFiles("template.tmpl")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	start := time.Now()
+	files := findTxtFiles(*dirPtr)
+	totalBytes := int64(0)
+
+	for _, fileName := range files {
+		fmt.Println("Generating HTML for:", fileName)
+		generateHTML(fileName, tmpl)
+		info, err := os.Stat(strings.TrimSuffix(fileName, ".txt") + ".html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		totalBytes += info.Size()
+	}
+
+	duration := time.Since(start)
+	fmt.Printf("\033[1;32mSuccess!\033[0m Generated \033[1m%d\033[0m pages (%.1fkB total) in %.2f seconds.\n", len(files), float64(totalBytes)/1024, duration.Seconds())
 }
 
