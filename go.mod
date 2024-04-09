ef5087012b88ddc9864f92b6754791710710ff88
Vithusha Ravirajan <vithusharavirajan@Vithus-MBP.local>
2024-04-09 00:25:50 -0400

    v1.2 complete

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module makesite
 
 go 1.21.1
+
+require github.com/gomarkdown/markdown v0.0.0-20240328165702-4d01890c35c0 // indirect
--- makesite.go
+++ makesite.go
@@ -8,94 +8,117 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-	"text/template"
+	"html/template"
 	"time"
+	"github.com/gomarkdown/markdown"
 )
 
-// findTxtFiles uses filepath.Walk to recursively find all .txt files in dirPath and its subdirectories.
-func findTxtFiles(dirPath string) ([]string, error) {
-	var files []string
-	err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if !info.IsDir() && strings.HasSuffix(info.Name(), ".txt") {
-			files = append(files, path)
-		}
-		return nil
-	})
-	return files, err
+func findFiles(dirPath string, includeMd bool) ([]string, error) {
+    var files []string
+    err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            return err
+        }
+        if !info.IsDir() && (strings.HasSuffix(info.Name(), ".txt") || (includeMd && strings.HasSuffix(info.Name(), ".md"))) {
+            files = append(files, path)
+        }
+        return nil
+    })
+    return files, err
 }
 
-// generateHTML reads a .txt file, extracts the title and content, and uses a template to generate an HTML file.
-func generateHTML(fileName string, tmpl *template.Template) {
-	file, err := os.Open(fileName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
+func generateHTML(fileName string, tmpl *template.Template, outputFileName string) {
+    
+    file, err := os.Open(fileName)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	scanner.Scan() // Reads the first line, which is the title
-	title := scanner.Text()
+    
+    scanner := bufio.NewScanner(file)
+    scanner.Scan() 
+    title := scanner.Text()
 
-	content := ""
-	for scanner.Scan() {
-		content += scanner.Text() + "\n"
-	}
+    var contentBuilder strings.Builder
+    for scanner.Scan() {
+        contentBuilder.WriteString(scanner.Text() + "\n")
+    }
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
 
-	data := struct {
-		Title   string
-		Content string
-	}{
-		Title:   title,
-		Content: content,
-	}
+    
+    var htmlContent []byte
+    if strings.HasSuffix(fileName, ".md") {
+        
+        htmlContent = markdown.ToHTML([]byte(contentBuilder.String()), nil, nil)
+    } else {
+        
+        
+        content := contentBuilder.String()
+        htmlContent = []byte("<p>" + strings.ReplaceAll(content, "\n", "</p><p>") + "</p>")
+    }
 
-	outputFileName := strings.TrimSuffix(fileName, ".txt") + ".html"
-	outFile, err := os.Create(outputFileName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer outFile.Close()
+    
+    data := struct {
+        Title   string
+        Content template.HTML 
+    }{
+        Title:   title,
+        Content: template.HTML(htmlContent),
+    }
 
-	err = tmpl.Execute(outFile, data)
-	if err != nil {
-		log.Fatal(err)
-	}
+    
+    outFile, err := os.Create(outputFileName)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer outFile.Close()
+
+    
+    err = tmpl.Execute(outFile, data)
+    if err != nil {
+        log.Fatal(err)
+    }
 }
 
 func main() {
-	dirPtr := flag.String("dir", ".", "Directory to search for .txt files")
-	flag.Parse()
+    dirPtr := flag.String("dir", ".", "Directory to search for text files")
+    mdPtr := flag.Bool("md", false, "Include markdown (.md) files")
+    flag.Parse()
 
-	tmpl, err := template.ParseFiles("template.tmpl")
-	if err != nil {
-		log.Fatal(err)
-	}
+    tmpl, err := template.ParseFiles("template.tmpl")
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	start := time.Now()
-	files, err := findTxtFiles(*dirPtr)
-	if err != nil {
-		log.Fatal(err)
-	}
+    start := time.Now()
+    files, err := findFiles(*dirPtr, *mdPtr)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	totalBytes := int64(0)
+    totalBytes := int64(0)
 
-	for _, fileName := range files {
-		fmt.Println("Generating HTML for:", fileName)
-		generateHTML(fileName, tmpl)
-		info, err := os.Stat(strings.TrimSuffix(fileName, ".txt") + ".html")
-		if err != nil {
-			log.Fatal(err)
-		}
-		totalBytes += info.Size()
-	}
+    for _, fileName := range files {
+        fmt.Println("Generating HTML for:", fileName)
 
-	duration := time.Since(start)
-	fmt.Printf("\033[1;32mSuccess!\033[0m Generated \033[1m%d\033[0m pages (%.1fkB total) in %.2f seconds.\n", len(files), float64(totalBytes)/1024, duration.Seconds())
+        
+        baseName := strings.TrimSuffix(fileName, filepath.Ext(fileName))
+        outputFileName := baseName + ".html"
+
+        generateHTML(fileName, tmpl, outputFileName)
+
+        
+        info, err := os.Stat(outputFileName)
+        if err != nil {
+            log.Fatal(err)
+        }
+        totalBytes += info.Size()
+    }
+
+    duration := time.Since(start)
+    fmt.Printf("\033[1;32mSuccess!\033[0m Generated \033[1m%d\033[0m pages (%.1fkB total) in %.2f seconds.\n", len(files), float64(totalBytes)/1024, duration.Seconds())
 }
